e76154c7528c7006bc8dd28e1902590f41594521
aguilanbon <aguilanbon@gmail.com>
2022-07-12 17:04:33 +0800

    header component for homepage

---
+++ src/components/Home/Avatar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Avatar() {
+  return (
+    <div className='mr-4 full'>
+        <div className='flex justify-between '>
+          <div className='flex'>
+            <div className='flex mr-2 items-center'>
+            <img src="./aloy.jpg" alt="" className='w-8 h-8 rounded-full object-cover' />
+          </div>
+          <div className='flex flex-col justify-center'>
+            <h1 className='text-sm font-semibold leading-3'>Aloy Zero</h1>
+            <h2 className='text-sm font semibold text-twittergray'>@zeroaloy</h2>
+          </div>
+          </div>
+          <div>
+            <svg viewBox="0 0 24 24" aria-hidden="true" class="fill-twitterwhite w-5 mr-4 r-1nao33i r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><circle cx="5" cy="12" r="2"></circle><circle cx="12" cy="12" r="2"></circle><circle cx="19" cy="12" r="2"></circle></g></svg>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Avatar
