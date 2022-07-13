a8001fa8dca22a1a45479a4189e3484119287db7
aguilanbon <aguilanbon@gmail.com>
2022-07-13 15:39:29 +0800

    fixed header

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -4,8 +4,8 @@ import Avatar from './Avatar'
 
 function Header() {
   return (
-    <div className='w-4/12 h-screen sticky flex flex-col items-end text-twitterwhite pb-6'>
-      <div className='w-52 h-full flex flex-col justify-evenly'>
+    <div className='w-4/12 h-screen flex flex-col items-end text-twitterwhite'>
+      <div className='w-52 h-full flex flex-col justify-evenly fixed pb-6'>
       <Links />
       <Avatar />
       </div>
