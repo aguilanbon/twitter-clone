af84736fbd3703f13b85bd5ee289b15603080a86
aguilanbon <aguilanbon@gmail.com>
2022-07-14 17:36:24 +0800

    footer

--- src/components/Home/Sidebar.jsx
+++ src/components/Home/Sidebar.jsx
@@ -5,7 +5,7 @@ import TrendCard from './TrendCard'
 
 function Sidebar() {
   return (
-    <div className='w-auto h-auto flex flex-col pl-4 mr-10'>
+    <div className='w-auto h-auto flex flex-col pl-4 mr-10 mb-36'>
       <div className='h-12 flex fixed items-center bg-black top-0'>
         <input type="text" className='h-8 pl-10 py-1 rounded-full bg-twitterbggray w-72 text-twitterwhite outline-none border-1 text-xs focus:outline-twitterblue focus:border-0' />
         <svg viewBox="0 0 24 24" aria-hidden="true" className="fill-twittergray absolute ml-2 h-4 pl-1 r-1bwzh9t r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-4wgw6l r-f727ji r-bnwqim r-1plcrui r-lrvibr"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
@@ -28,6 +28,17 @@ function Sidebar() {
         <h1 className='text-twitterwhite p-4 font-bold' >Who to follow</h1>
         <FollowSuggestions />
         <ShowMore />
+      </div>
+      <div className='flex flex-col w-72 text-twittergray mt-3 text-xxs'>
+        <div className='flex flex-wrap'>
+        <a href="/" className='mx-1'>Terms of Service</a>
+        <a href="/" className='mx-1'>Privacy Policy</a>
+        <a href="/" className='mx-1'>Cookie Policy</a>
+        <a href="/" className='mx-1'>Accessibility</a>
+        <a href="/" className='mx-1'>Ads info</a>
+        <a href="/" className='mx-1'>More ...</a>
+        </div>
+        <p>&copy; 2022 Twitter, Inc.</p>
       </div>
     </div>
   )
