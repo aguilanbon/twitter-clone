79209fa0dc4c60168764563f027ae7076b8a6d38
aguilanbon <aguilanbon@gmail.com>
2022-07-16 20:23:53 +0800

    hover effects oon tweet comments

--- src/components/Home/TweetOptions.jsx
+++ src/components/Home/TweetOptions.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TweetOptions() {
+  const [isComments, setIsComments] = useState(false)
+
   return (
     <div className='flex items-center justify-between w-10/12 ml-3 mt-3 text-xs'>
-      <span className='flex text-twittergray'>
-        <svg viewBox="0 0 24 24" aria-hidden="true" className="fill-twittergray h-4 rounded-full r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"><g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g></svg>
-        <span className='ml-2'>
+      <span className='flex text-twittergray items-center'>
+        <div className='p-1 rounded-full hover:bg-twitterblue hover:bg-opacity-30 hover:text-twitterblue hover:fill-blue-600' onMouseEnter={() => setIsComments(true)} onMouseLeave={() => setIsComments(false)}>
+          <svg viewBox="0 0 24 24" aria-hidden="true" className={!isComments ? "fill-twittergray h-4 rounded-full r-4 qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi" : "fill-twitterblue h-4 rounded-full r-4 qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"}><g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g></svg>
+        </div>
+        <span className={!isComments ? 'ml-2' : 'ml-2 text-twitterblue'}>
           3,444
         </span>
       </span>
