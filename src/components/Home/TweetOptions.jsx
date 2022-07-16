fd186f4f1ed60cca7d4401f6a67c4a8820b189d2
aguilanbon <aguilanbon@gmail.com>
2022-07-16 20:34:19 +0800

    like hover state

--- src/components/Home/TweetOptions.jsx
+++ src/components/Home/TweetOptions.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function TweetOptions() {
   const [isComments, setIsComments] = useState(false)
   const [isRetweet, setIsRetweet] = useState(false)
+  const [isLike, setIsLike] = useState(false)
 
   return (
     <div className='flex items-center justify-between w-10/12 ml-3 mt-3 text-xs'>
@@ -23,8 +24,10 @@ function TweetOptions() {
         </span>
       </span>
       <span className='flex text-twittergray items-center'>
-        <svg viewBox="0 0 24 24" aria-hidden="true" class="fill-twittergray h-4 rounded-full r-4 r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
-        <span className='ml-2'>
+        <div className='p-1 rounded-full hover:bg-pink-400 hover:bg-opacity-10' onMouseEnter={() => setIsLike(true)} onMouseLeave={() => setIsLike(false)}>
+        <svg viewBox="0 0 24 24" aria-hidden="true" class={!isLike ? "fill-twittergray h-4 rounded-full r-4 r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi" : "fill-pink-700 h-4 rounded-full r-4 r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"}><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
+        </div>
+        <span className={!isLike ? 'ml-2' : 'ml-2 text-pink-700'}>
           27.7k
         </span>
       </span>
