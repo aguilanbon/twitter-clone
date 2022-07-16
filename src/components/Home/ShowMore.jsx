e4b44238b75d479036b1dc665df7d5ee63fb32d0
aguilanbon <aguilanbon@gmail.com>
2022-07-16 20:45:59 +0800

    show more hover state

--- src/components/Home/ShowMore.jsx
+++ src/components/Home/ShowMore.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ShowMore() {
   return (
-    <div className='flex bg-twitterbggray rounded-b-xl p-4'>
+    <div className='flex bg-twitterbggray hover:bg-twitterhvgray rounded-b-xl p-4'>
         <a href="/" className='text-twitterblue text-xs'>Show more</a>
     </div>
   )
