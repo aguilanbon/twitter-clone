83da5fee8667e8578d821cbfb942f06dfed33279
aguilanbon <aguilanbon@gmail.com>
2022-07-19 16:47:29 +0800

    hidden trending topics on medium devices

--- src/components/Home/HomeFooter.jsx
+++ src/components/Home/HomeFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeFooter() {
   return (
-    <div className='flex flex-col w-72 text-twittergray mt-3 text-xxs'>
+    <div className='flex flex-col xl:w-72 lg:w-60 w-auto text-twittergray mt-3 text-xxs'>
         <div className='flex flex-wrap'>
         <a href="/" className='mx-1 hover:border-b hover:border-twittergray h-4'>Terms of Service</a>
         <a href="/" className='mx-1 hover:border-b hover:border-twittergray h-4'>Privacy Policy</a>
