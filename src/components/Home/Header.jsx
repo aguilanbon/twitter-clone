346183823db1b880887ce8321491b5e3c168d34c
aguilanbon <aguilanbon@gmail.com>
2022-07-19 16:57:29 +0800

    better spacing on large screens

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -4,7 +4,7 @@ import Avatar from './Avatar'
 
 function Header() {
   return (
-    <div className='w-2/12 lg:w-3/12 md:w-1/4 h-screen flex flex-col items-end text-twitterwhite'>
+    <div className='w-2/12 xl:w-4/12 md:w-1/4 h-screen flex flex-col items-end text-twitterwhite'>
       <div className='lg:w-52 w-auto h-full flex flex-col justify-evenly fixed pb-6'>
       <Links />
       <Avatar />
