ddb5e1e5ef59638e9f85732c6b223b2416be6c1e
aguilanbon <aguilanbon@gmail.com>
2022-07-22 20:17:48 +0800

    include messages widget on main page

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Feed from './Feed'
+import MessagesWidget from './MessagesWidget'
 import Sidebar from './Sidebar'
 
 function MiddleContainer() {
   return (
     <div className='xl:w-8/12 w-full h-auto flex'>
-        <div className='flex w-full'>
+        <div className='flex w-full static'>
           <Feed />
           <Sidebar />
+          <MessagesWidget />
         </div>
     </div>
   )
--- src/components/Home/MessagesWidget.jsx
+++ src/components/Home/MessagesWidget.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MessagesWidget() {
   return (
-    <div>
+    <div className='absolute bg-red-500 w-72 h-10 bottom-0 right-5'>
         
     </div>
   )
