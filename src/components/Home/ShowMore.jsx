26cdcb19ae7a4382bf18c84344a1018d7bd54a76
aguilanbon <aguilanbon@gmail.com>
2022-07-25 12:41:33 +0800

    increase width and typography of side bar

--- src/components/Home/ShowMore.jsx
+++ src/components/Home/ShowMore.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ShowMore() {
   return (
-    <div className='flex bg-twitterbggray hover:bg-twitterhvgray rounded-b-xl p-4 xl:w-72 lg:w-60 w-auto'>
+    <div className='flex bg-twitterbggray hover:bg-twitterhvgray rounded-b-xl p-4 xl:w-80 lg:w-60 w-auto'>
         <a href="/" className='text-twitterblue text-xs'>Show more</a>
     </div>
   )
--- src/components/Home/TrendCard.jsx
+++ src/components/Home/TrendCard.jsx
@@ -4,9 +4,9 @@ function TrendCard() {
   return (
     <div className='w-full  flex py-2 justify-between px-2 cursor-pointer bg-twitterbggray hover:bg-twitterhvgray'>
         <div className='flex flex-col pl-2'>
-            <p className='text-twittergray text-xxs'>Trending in Philippines</p>
-            <h2 className='text-twitterwhite font-bold text-xs'>Department of Health</h2>
-            <p className='text-twittergray text-xxs'>3,491 Tweets</p>
+            <p className='text-twittergray text-xs'>Trending in Philippines</p>
+            <h2 className='text-twitterwhite font-bold text-sm'>Department of Health</h2>
+            <p className='text-twittergray text-xs'>3,491 Tweets</p>
         </div>
         <div className='flex pr-2'>
             <svg viewBox="0 0 24 24" aria-hidden="true" className="h-4 fill-twitterwhite r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"><g><circle cx="5" cy="12" r="2"></circle><circle cx="12" cy="12" r="2"></circle><circle cx="19" cy="12" r="2"></circle></g></svg>
