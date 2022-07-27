4a19ff46780eee1e7903d5e218f9b065c95b05df
aguilanbon <aguilanbon@gmail.com>
2022-07-27 21:34:25 +0800

    add modal into overlay

---
+++ src/components/Home/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <div className='w-100 h-96 bg-black z-50 rounded-xl'>
+        Modal
+    </div>
+  )
+}
+
+export default Modal
--- src/components/Home/Modal/Overlay.jsx
+++ src/components/Home/Modal/Overlay.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Modal from './Modal'
 
 function Overlay() {
   return (
-    <div className='w-full min-h-screen bg-slate-500 opacity-40 fixed z-50'>
-        
+    <div className='w-full min-h-screen bg-slate-500 bg-opacity-40 fixed z-40 flex items-center justify-center'>
+      <Modal />
     </div>
   )
 }
